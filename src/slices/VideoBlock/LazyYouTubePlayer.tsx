9b01f9d8108467c8be59ab3104b0f2c3b6fa4611
Anubhav Agrawal <anubhav@UTXOTechs-MacBook-Air.local>
2025-01-19 22:16:15 +0530

    first-commit

---
+++ src/slices/VideoBlock/LazyYouTubePlayer.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { KeyTextField } from "@prismicio/client";
+import { useEffect, useRef, useState } from "react";
+
+type VideoProps = {
+  youTubeID: KeyTextField;
+};
+
+export function LazyYouTubePlayer({ youTubeID }: VideoProps) {
+  const [isInView, setIsInView] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const currentContainerRef = containerRef.current;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsInView(true);
+        }
+      },
+      { threshold: 0, rootMargin: "1500px" }
+    );
+
+    if (currentContainerRef) {
+      observer.observe(currentContainerRef);
+    }
+
+    return () => {
+      if (currentContainerRef) {
+        observer.unobserve(currentContainerRef);
+      }
+    };
+  });
+
+  return (
+    <div className="relative h-full w-full" ref={containerRef}>
+      {isInView && (
+        <iframe
+          src={`https://www.youtube.com/embed/yn-TfAzobDI?si=5hqd5I6I3x5eZFNv&autoplay=1&mute=1`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          className="pointer-events-none h-full w-full border-0"
+        />
+      )}
+    </div>
+  );
+}
